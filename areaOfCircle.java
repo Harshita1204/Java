5a0df1b73d3becf6b67a71f9fb9b90318c7101fb
Harshita1204 <harshitak1204@gmail.com>
2026-01-06 23:40:22 +0530

    Create areaOfCircle.java

---
+++ areaOfCircle.java
@@ -0,0 +1,10 @@
+package basic_things;
+
+public class areaOfCircle {
+    static void main(String[] args) {
+        double radius = 3.8;
+        double area = 3.1415 * radius  * radius;
+        System.out.println(area);
+
+    }
+}
