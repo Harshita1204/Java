1b389ca31fcbd2304a19c85cb226340fa3c7a833
Harshita1204 <harshitak1204@gmail.com>
2026-01-06 23:02:46 +0530

    printing_numbers.java

---
+++ printing_numbers.java
@@ -0,0 +1,15 @@
+package basic_things;
+
+public class printingNumbers {
+    static void main(String[] args) {
+      // arithmetic operations
+        System.out.println(69);
+        System.out.println(69+1);
+        System.out.println(72/2);
+        System.out.println(69%2);
+        System.out.println(23*3);
+        System.out.println(987-234);
+        System.out.println("69+23"); // it will print 69+23 as it is as it is in inverted commas, it acts as a string not integers
+        
+    }
+}
