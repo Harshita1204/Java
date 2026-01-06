8ec89ccbc6ec638baa396604c9613b7eaa534beb
Harshita1204 <harshitak1204@gmail.com>
2026-01-06 23:43:58 +0530

    Create volumeOfSphere.java

---
+++ volumeOfSphere.java
@@ -0,0 +1,11 @@
+package basic_things;
+
+public class volumeOfSphere {
+    static void main(String[] args) {
+        double r = 3;
+        double volume = (4/3 * 3.14 * r*r*r);
+        System.out.println(volume);
+
+
+    }
+}
