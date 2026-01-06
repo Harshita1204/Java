06c3f452ae3a08e799efd4f4c999a6f1bfd547b8
Harshita1204 <harshitak1204@gmail.com>
2026-01-06 22:44:33 +0530

    Create java_basic_1.java

---
+++ java_basic_1.java
@@ -0,0 +1,7 @@
+package basic_things;
+
+public class helloYashna {
+    public static void main(String[] args) { //string -> S capital
+        System.out.println("Hello Yashna");  //system -> S capital
+    }
+}
