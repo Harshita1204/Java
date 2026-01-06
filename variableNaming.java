9f6c5167f282fd1afb33462c882f0a9e7c4ad657
Harshita1204 <harshitak1204@gmail.com>
2026-01-07 00:08:30 +0530

    Create variableNaming.java

---
+++ variableNaming.java
@@ -0,0 +1,15 @@
+package basic_things;
+
+public class variableNaming {
+    static void main(String[] args) {
+        // we can start a variable name with $ or _ , rest of the special characters we cant use
+        int xy = 3;
+        int name = 4;
+        int _x = 23;
+        int $y = 12;
+        System.out.println(xy); //3
+        System.out.println(name); //4
+        System.out.println(_x); //23
+        System.out.println($y); //12
+    }
+}
