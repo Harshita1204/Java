a60a9bc9513c58dd325f6855698382e235328013
Harshita1204 <harshitak1204@gmail.com>
2026-01-06 23:14:58 +0530

    Create variableBasics.java

---
+++ variableBasics.java
@@ -0,0 +1,13 @@
+package basic_things;
+
+public class variableBasics {
+    static void main(String[] args) {
+        int x= 5; 
+        System.out.println(x); //5
+        System.out.println(x+7); //12
+        int y= 20; 
+        System.out.println(y); //20
+        System.out.println(x+y); //25
+        System.out.println(x*y); //100
+    }
+}
